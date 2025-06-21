6279208176ce5d02824055a59aef714d128fae59
Guyutongxue <guyutongxue@163.com>
2025-06-21 18:12:22 +0800

    fix stupid MSVC

--- v8/src/heap/cppgc/marking-state.h
+++ v8/src/heap/cppgc/marking-state.h
@@ -342,7 +342,7 @@ class MutatorMarkingState final : public BasicMarkingState {
   ~MutatorMarkingState() override = default;
 
   inline bool MarkNoPush(HeapObjectHeader& header) {
-    return MutatorMarkingState::BasicMarkingState::MarkNoPush(header);
+    return this->MutatorMarkingState::BasicMarkingState::MarkNoPush(header);
   }
 
   inline void ReTraceMarkedWeakContainer(cppgc::Visitor&, HeapObjectHeader&);
